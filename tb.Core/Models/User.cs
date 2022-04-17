47be70824d9d17637b79f66d100c5457a89db5b6
modiel <diane.e.morrison@gmail.com>
2022-04-17 21:26:03 +0100

    password

--- tb.Core/Models/User.cs
+++ tb.Core/Models/User.cs
@@ -44,6 +44,7 @@ namespace tb.Core.Models
         [Required] 
         [EmailAddress] 
         public string Email { get; set; } = "";
+        [Required]
         public string Password { get; set; } = "";
         public Role Role { get; set; } = Role.Pupil;
 
